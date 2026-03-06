18d43d893e021513446bda2c80eb61f547c16827
Vinnicius Magione <vinniciussoares@hotmail.com>
2026-03-06 18:52:44 -0300

    Update main.go
    Update main.go

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	var folder string
-	var email string
+	var folder string = "C:\Users\vinni\OneDrive\Documentos"
+	var email string = "vinniciussoares@hotmail.com"
 
 	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
 	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
@@ -18,6 +18,6 @@ func main() {
 		return
 	}
 
-	//stats(email)
+	stats(email)
 
 }
