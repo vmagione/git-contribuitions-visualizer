1b20ce8c1be2209f9ae9e66970abdddfb3c70f55
Vinnicius Magione <vinniciussoares@hotmail.com>
2026-03-06 18:46:43 -0300

    Update prints stats
    Update prints stats

--- stats.go
+++ stats.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"sort"
 	"time"
 
 	"gopkg.in/src-d/go-git.v4"
@@ -9,6 +11,9 @@ import (
 
 const daysInLastSixMonths = 183
 const outOfRange = 99999
+const weeksInLastSixMonths = 26
+
+type column []int
 
 // stats generates a nice graph of your Git contributions
 
@@ -94,4 +99,164 @@ func countDaysSinceDate(date time.Time) int {
 		}
 	}
 	return days
+}
+
+// calcOffset determines and returns the amount of days missing to fill
+// the last row of the stats graph
+func calcOffset() int {
+	var offset int
+	weekday := time.Now().Weekday()
+	switch weekday {
+	case time.Sunday:
+		offset = 7
+	case time.Monday:
+		offset = 6
+	case time.Tuesday:
+		offset = 5
+	case time.Wednesday:
+		offset = 4
+	case time.Thursday:
+		offset = 3
+	case time.Friday:
+		offset = 2
+	case time.Saturday:
+		offset = 1
+	}
+	return offset
+}
+
+// printCommitsStats prints the commits stats
+func printCommitsStats(commits map[int]int) {
+	keys := sortMapIntSlice(commits)
+	cols := buildCols(keys, commits)
+	printCells(cols)
+}
+
+// sortMapIntoSlice returns a slice of indexes of a map, ordered
+func sortMapIntSlice(m map[int]int) []int {
+	// order map
+	// To store the keys in slice in sorted order
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	return keys
+}
+
+// buildCols generates a map with rows and columns ready to be printed to screen
+func buildCols(keys []int, commits map[int]int) map[int]column {
+	cols := make(map[int]column)
+	col := column{}
+	for _, k := range keys {
+		week := int(k / 7)  // 26,25...1
+		dayinweek := k % 7  // 0,1,2,3,4,5,6
+		if dayinweek == 0 { // reset
+			col = column{}
+		}
+		col = append(col, commits[k])
+		if dayinweek == 6 {
+			cols[week] = col
+		}
+	}
+	return cols
+}
+
+// printCells prints the cells of the graph
+func printCells(cols map[int]column) {
+	printMonths()
+	for j := 6; j > 0; j-- {
+		for i := weeksInLastSixMonths + 1; i >= 0; i-- {
+			if i == weeksInLastSixMonths+1 {
+				printDayCol(j)
+			}
+			if col, ok := cols[i]; ok {
+				// special case today
+				if i == 0 && j == calcOffset()-1 {
+					printCell(col[j], true)
+					continue
+				} else {
+					if len(col) > j {
+						printCell(col[j], false)
+						continue
+					}
+				}
+			}
+			printCell(0, false)
+		}
+		fmt.Printf("\n")
+	}
+}
+
+// printMonths prints the month names in the first line, determining when the month
+// changed between switching weeks
+func printMonths() {
+	week := getBeginningOfDay(time.Now()).Add(-(daysInLastSixMonths * time.Hour * 24))
+	month := week.Month()
+	fmt.Printf("         ")
+	for {
+		if week.Month() != month {
+			fmt.Printf("%s ", week.Month().String()[:3])
+			month = week.Month()
+		} else {
+			fmt.Printf("    ")
+		}
+		week = week.Add(7 * time.Hour * 24)
+		if week.After(time.Now()) {
+			break
+		}
+	}
+	fmt.Printf("\n")
+}
+
+// printDayCol given the day number (0 is Sunday) prints the day name,
+// alternating the rows (prints just 2,4,6)
+func printDayCol(day int) {
+	out := "     "
+	switch day {
+	case 0:
+		out = " Dom "
+	case 1:
+		out = " Seg "
+	case 2:
+		out = " Ter "
+	case 3:
+		out = " Qua "
+	case 4:
+		out = " Qui "
+	case 5:
+		out = " Sex "
+	case 6:
+		out = " Sab "
+	}
+	fmt.Printf(out)
+}
+
+// printCell given a cell value prints it with a different format
+// based on the value amount, and on the `today` flag.
+func printCell(val int, today bool) {
+	escape := "\033[0;37;30m"
+	switch {
+	case val > 0 && val < 5:
+		escape = "\033[1;30;47m"
+	case val >= 5 && val < 10:
+		escape = "\033[1;30;47m"
+	case val >= 10:
+		escape = "\033[1;30;47m"
+	}
+	if today {
+		escape = "\033[1;37;45m"
+	}
+	if val == 0 {
+		fmt.Printf("%s", escape+"  - "+"\033[0m")
+		return
+	}
+	str := "  %d "
+	switch {
+	case val >= 10:
+		str = " %d "
+	case val >= 100:
+		str = "%d "
+	}
+	fmt.Printf(escape+str+"\033[0m", val)
 }
