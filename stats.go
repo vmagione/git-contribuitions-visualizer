8bf94c083c2f901937561e7ae3b0eb5e3c8a2db1
Vinnicius Magione <vinniciussoares@hotmail.com>
2026-03-06 18:21:49 -0300

    Calculate Days and times
    Calculate Days and times

--- stats.go
+++ stats.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"time"
+
 	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
+
+const daysInLastSixMonths = 183
+const outOfRange = 99999
 
 // stats generates a nice graph of your Git contributions
 
@@ -31,9 +37,61 @@ func processRepositories(email string) map[int]int {
 // fillCommits given a repository found in `path`, gets the commits and
 // puts them in the `commits` map, returning it when completed
 func fillCommits(email string, path string, commits map[int]int) map[int]int {
+
 	// instantiate a git repo object from path
 	repo, err := git.PlainOpen(path)
 	if err != nil {
 		panic(err)
 	}
+
+	// get the HEAD reference
+	ref, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+
+	// get the commits history starting from HEAD
+	iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		panic(err)
+	}
+
+	// iterate the commits
+	offset := calcOffset()
+	err = iterator.ForEach(func(c *object.Commit) error {
+		daysAgo := countDaysSinceDate(c.Author.When) + offset
+		if c.Author.Email != email {
+			return nil
+		}
+		if daysAgo != outOfRange {
+			commits[daysAgo]++
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return commits
+
+}
+
+// getBeginningOfDay given a time.Time calculates the start time of that day
+func getBeginningOfDay(t time.Time) time.Time {
+	year, mont, day := t.Date()
+	startOfDay := time.Date(year, mont, day, 0, 0, 0, 0, t.Location())
+	return startOfDay
+}
+
+// countDaysSinceDate counts how many days passed since the passed `date`
+func countDaysSinceDate(date time.Time) int {
+	days := 0
+	now := getBeginningOfDay(time.Now())
+	for date.Before(now) {
+		date = date.Add(time.Hour * 24)
+		days++
+		if days > daysInLastSixMonths {
+			return outOfRange
+		}
+	}
+	return days
 }
