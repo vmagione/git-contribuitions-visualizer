b80a6c916771c781ce9878cbd14a17ba2c87dbff
Vinnicius Magione <vinniciussoares@hotmail.com>
2026-03-05 21:28:27 -0300

    Create scan.go
    Create Scaning of folders and subfolders for git repositories

--- main.go
+++ main.go
@@ -4,19 +4,6 @@ import (
 	"flag"
 )
 
-// scan given a path crawls it and its subfolders
-// searching for Git repositories
-
-func scan(path string) {
-	print("scan")
-}
-
-// stats generates a nice graph of your Git contributions
-
-func stats(email string) {
-	print("stats")
-}
-
 func main() {
 	var folder string
 	var email string
---
+++ stats.go
@@ -0,0 +1,7 @@
+package main
+
+// stats generates a nice graph of your Git contributions
+
+func stats(email string) {
+	print("stats")
+}
